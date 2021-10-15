295b11edd5d31049c83ff98397412d8d58a29eb5
Genevieve Michaud <migenevieve@gmail.com>
2021-10-15 17:24:05 +0200

    code Décor : routes, controller, views

--- app/models/decor.rb
+++ app/models/decor.rb
@@ -2,4 +2,7 @@ class Decor < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  CATEGORY = %w[vintage original campagnard contemporain industriel neutre artistique]
+  LUMINOSITY = %w[peu moyen très]
+  DECOR_EXPOSITION = %w[nord sud est ouest]
 end
